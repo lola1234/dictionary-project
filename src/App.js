0fb2842a30c1f9e5cf517bc1e0444ac91e42c5bf
lola1234 <101635306+lola1234@users.noreply.github.com>
2022-07-24 23:11:33 -0400

    Added dictionary component, set up search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header"> Dictionary</header>
+      <main>
+        <Dictionary />
+      </main>
+
+      <footer className="App-footer">
+        <small>
+          Coded by{" "}
+          <a
+            href="https://github.com/lola1234/dictionary-project"
+            target="blank"
+          >
+            Linora Imeri{" "}
+          </a>
+        </small>
+      </footer>
     </div>
   );
 }
-
-export default App;
